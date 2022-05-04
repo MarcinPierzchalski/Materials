d1ea39bc627aef32b61a55c0aca8b869ef5f2dfd
Marcin P <mpierzchalski@gmail.com>
2022-05-04 22:52:02 +0200

    MaterialsTwo

--- Class28/dnd/js/main.js
+++ Class28/dnd/js/main.js
@@ -3,11 +3,18 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}/`
+  
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        document.querySelector('ul').innerHTML = '';
+          console.log(data)
+          document.querySelector('h3').innerText = data.classes[0].name
+          data.subclasses.forEach(element => {const li = document.createElement("li"); 
+                                              li.textContent = element.name;
+                                              document.querySelector('ul').appendChild(li);})
        
       })
       .catch(err => {
